fbf5856e073d436793394fb6ee542d9defd02c01
Jason Deng <jgjdeng@gmail.com>
2021-12-15 00:10:59 -0600

    Starting on project

---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import "./Card.css";
+
+function Card(props) {
+    const classes = "card " + props.className;
+
+    return <div className={classes}>{props.children}</div>  // props.children allows props pass through
+}
+
+export default Card;
