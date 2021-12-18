b448c3849669b5ba89188d59dbbd3bead341607d
Jason Deng <jgjdeng@gmail.com>
2021-12-17 21:50:26 -0600

    Using Stateful Lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,27 +6,29 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses(props) {
-    const [filteredYear, setFilteredYear] = useState("2020");
-    
-    const filterChangeHandler = selectedYear => {
-      
-        setFilteredYear(selectedYear);
-    }
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-    return (
-        <div>
-            <Card className="expenses">
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map(expense => (
-                    <ExpenseItem 
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
-            </Card>
-        </div>
-    )
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
 }
 
 export default Expenses;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,9 @@
 import "./Card.css";
 
 function Card(props) {
-    const classes = "card " + props.className;
+  const classes = "card " + props.className;
 
-    return <div className={classes}>{props.children}</div>  // props.children allows props pass through
+  return <div className={classes}>{props.children}</div>; // props.children allows props pass through
 }
 
 export default Card;
